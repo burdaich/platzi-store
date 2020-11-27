76d83458a40494de10bbae05d2b1f5f1b7fc4b13
edorta <burdaich@gmail.com>
2020-11-27 12:04:55 +0100

    modifies for work with angular forms

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,12 +2,13 @@ export const environment = {
   production: true,
   url_api: 'https://store.nicobytes.site/api',
   firebase: {
-    apiKey: 'AIzaSyCN599W6_flBEpe0xrcuBlGPRW3OKzDr7o',
-    authDomain: 'platzi-store-forms.firebaseapp.com',
-    databaseURL: 'https://platzi-store-forms.firebaseio.com',
-    projectId: 'platzi-store-forms',
-    storageBucket: 'platzi-store-forms.appspot.com',
-    messagingSenderId: '783669493891',
-    appId: '1:783669493891:web:f973c1ba9bb213e2c75bb6'
+    apiKey: 'AIzaSyCloE1bW-Sy6DPG2E2_3jgzzEbXfmC9Q3k',
+  authDomain: 'platzi-store-62ee3.firebaseapp.com',
+  databaseURL: 'https://platzi-store-62ee3.firebaseio.com',
+  projectId: 'platzi-store-62ee3',
+  storageBucket: 'platzi-store-62ee3.appspot.com',
+  messagingSenderId: '1015305089783',
+  appId: '1:1015305089783:web:580c537571a11b8d6dd9f2',
+  measurementId: 'G-360KGCV4J2'
   }
 };
